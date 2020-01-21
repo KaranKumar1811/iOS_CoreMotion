bb4b26622ff809cb188879dc55e52b8b9407fc40
Karan <kumarkay.ca@gmail.com>
2020-01-21 16:14:28 -0500

    Initial Commit

---
+++ iOS_CoreMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_CoreMotion
+//
+//  Created by Sandeep Jangra on 2020-01-21.
+//  Copyright © 2020 Karan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
